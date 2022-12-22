266eb32e9ac8c759adf167e2768eb6bfd3a811e5
SK ISMILE <101392872+skismile@users.noreply.github.com>
2022-12-22 14:57:56 +0530

    add

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const app=express()
 const cors=require("cors")
 require("dotenv").config()
 const mongoose=require("mongoose")
-
-const RandomWord=require("./Router/Randomword.route")
 const QuizRouter=require("./Router/Quiz.Router")
 const PORT=process.env.PORT||8080
 const MONGO_URL=process.env.MONGO_URL
@@ -12,7 +10,7 @@ app.use(express.urlencoded({extended : true}))
 app.use(cors());
 app.use(express.json())
 
-app.use("/randomword",RandomWord)
+
 app.use("/quiz",QuizRouter)
 mongoose.set("strictQuery", false);
 app.get('/',(req,res)=>{res.send('hello')})
